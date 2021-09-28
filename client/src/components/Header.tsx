10706a0c8be39d80b674829c19241b34c060936e
TyroneWilliams <itenory@gmail.com>
2021-09-28 19:47:36 -0400

    Created auth nav

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,70 @@
 import * as React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import RecipeSearch from './RecipeSearch';
+import { useAuthContext } from '../context/auth';
 import './styles/header.css';
+
+const AuthNav = ({
+  userId,
+  signout,
+}: {
+  userId: String;
+  signout: Function;
+}) => (
+  <ul className="header__list">
+    <li className="header__item header__item--title">
+      <Link className="header__link" to="/">
+        Reshipi
+        <span className="header__text-alt"> Bukku</span>
+      </Link>
+    </li>
+
+    <li className="header__item">
+      <Link className="header__link" to="/explore/popular">
+        Recipes
+      </Link>
+    </li>
+
+    <li className="header__item">
+      <Link className="header__link" to="/dashboard">
+        Dashboard
+      </Link>
+    </li>
+
+    <li className="header__item">
+      <Link className="header__link" to={`/account/profile/${userId}`}>
+        Profile
+      </Link>
+    </li>
+
+    <li className="header__item">
+      <Link className="header__link" to="/account/settings">
+        Settings
+      </Link>
+    </li>
+
+    <li className="header__item">
+      <button
+        className="header__link"
+        type="button"
+        onClick={() => {
+          signout();
+        }}
+      >
+        Signout
+      </button>
+    </li>
+  </ul>
+);
 
 const Header = () => {
   const [menuActive, setMenuActive] = React.useState<Boolean>(false);
   const location = useLocation();
+  const { state, signout } = useAuthContext();
 
   React.useEffect(() => {
-    if (menuActive) setMenuActive(false);
-  }, [location.pathname, menuActive]);
+    setMenuActive(false);
+  }, [location.pathname]);
 
   return (
     <header className={`header ${menuActive ? 'menu__active' : ''}`}>
@@ -24,32 +79,36 @@ const Header = () => {
       </button>
 
       <nav className="header__nav" role="navigation">
-        <ul className="header__list">
-          <li className="header__item header__item--title">
-            <Link className="header__link" to="/">
-              Reshipi
-              <span className="header__text-alt"> Bukku</span>
-            </Link>
-          </li>
+        {state.authenticated ? (
+          <AuthNav userId={state.id || ''} signout={signout} />
+        ) : (
+          <ul className="header__list">
+            <li className="header__item header__item--title">
+              <Link className="header__link" to="/">
+                Reshipi
+                <span className="header__text-alt"> Bukku</span>
+              </Link>
+            </li>
 
-          <li className="header__item">
-            <Link className="header__link" to="/recipes/popular">
-              Recipes
-            </Link>
-          </li>
+            <li className="header__item">
+              <Link className="header__link" to="/explore/popular">
+                Recipes
+              </Link>
+            </li>
 
-          <li className="header__item">
-            <Link className="header__link" to="/signin">
-              Signin
-            </Link>
-          </li>
+            <li className="header__item">
+              <Link className="header__link" to="/signin">
+                Signin
+              </Link>
+            </li>
 
-          <li className="header__item">
-            <Link className="header__link" to="/signup">
-              Signup
-            </Link>
-          </li>
-        </ul>
+            <li className="header__item">
+              <Link className="header__link" to="/signup">
+                Signup
+              </Link>
+            </li>
+          </ul>
+        )}
       </nav>
 
       <div className="header__search">
