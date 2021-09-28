fc6aeb30d00931a7f414104473e94a41b25416e0
TyroneWilliams <itenory@gmail.com>
2021-09-28 19:46:53 -0400

    Updated routes

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -11,40 +11,54 @@ import FeedbackPage from './scenes/Feedback';
 import AboutPage from './scenes/About';
 import SupportPage from './scenes/Support';
 import SearchPage from './scenes/search/Search';
-import RecipeListPage from './scenes/recipes/Recipe';
 import RecipePage from './scenes/recipe/Recipe';
 import SettingsPage from './scenes/account/Settings';
 import CreateRecipePage from './scenes/account/CreateRecipe';
 import ProfilePage from './scenes/account/Profile';
-
-const MissingPage = () => <section className="missing">404</section>;
+import EditRecipePage from './scenes/recipe/EditRecipe';
+import MissingPage from './scenes/Missing';
+import PopularPage from './scenes/explore/Popular';
+import DashboardPage from './scenes/dashboard/Dashboard';
+import ManageRecipesPage from './scenes/dashboard/ManageRecipes';
+import { AuthProvider } from './context/auth';
+import AnalyticsPage from './scenes/dashboard/Analytics';
+import RecoveryPage from './scenes/auth/Recovery';
 
 export default () => (
   <Router>
-    <Header />
-    <main className="main">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/faq" component={FaqPage} />
-        <Route path="/started" component={StartedPage} />
-        <Route path="/feedback" component={FeedbackPage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/support" component={SupportPage} />
+    <AuthProvider>
+      <Header />
+      <main className="main">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/faq" component={FaqPage} />
+          <Route path="/started" component={StartedPage} />
+          <Route path="/feedback" component={FeedbackPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/support" component={SupportPage} />
 
-        <Route path="/signin" component={SigninPage} />
-        <Route path="/signup" component={SignupPage} />
+          <Route path="/signin" component={SigninPage} />
+          <Route path="/signup" component={SignupPage} />
+          <Route path="/recovery" component={RecoveryPage} />
 
-        <Route path="/search" component={SearchPage} />
-        <Route path="/recipes/:type" component={RecipeListPage} />
-        <Route path="/recipe/:id" component={RecipePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route exact path="/recipe/:recipeId" component={RecipePage} />
+          <Route path="/recipe/:recipeId/edit" component={EditRecipePage} />
 
-        <Route path="/account/settings" component={SettingsPage} />
-        <Route path="/account/recipe/create" component={CreateRecipePage} />
-        <Route path="/account/profile" component={ProfilePage} />
+          <Route path="/explore/popular" component={PopularPage} />
 
-        <Route component={MissingPage} />
-      </Switch>
-    </main>
-    <Footer />
+          <Route exact path="/dashboard" component={DashboardPage} />
+          <Route path="/dashboard/manage" component={ManageRecipesPage} />
+          <Route path="/dashboard/analytics" component={AnalyticsPage} />
+
+          <Route path="/account/settings" component={SettingsPage} />
+          <Route path="/account/recipe/create" component={CreateRecipePage} />
+          <Route path="/account/profile/:userId" component={ProfilePage} />
+
+          <Route component={MissingPage} />
+        </Switch>
+      </main>
+      <Footer />
+    </AuthProvider>
   </Router>
 );
