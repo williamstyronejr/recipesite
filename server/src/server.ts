2fb1718a88b8588c88c2db3be4aa1ae3fd0aaa1f
TyroneWilliams <itenory@gmail.com>
2021-09-29 12:37:17 -0400

    Now using apollo server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,40 @@
-import { createServer } from 'http';
-import app from './services/app';
+import http from 'http';
+import path from 'path';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { graphqlUploadExpress } from 'graphql-upload';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 import logger from './services/logger';
-import { setUpRedis } from './services/redis';
+import db from './models';
 
-const { REDIS_HOST, REDIS_PORT, REDIS_URL } = process.env;
+// import { setUpRedis } from './services/redis';
+
+// const { REDIS_HOST, REDIS_PORT, REDIS_URL } = process.env;
 
 export const startServer = async (IP: string, PORT: number): Promise<void> => {
-  const httpServer = createServer(app);
-  try {
-    await setUpRedis(
-      REDIS_HOST,
-      REDIS_PORT ? parseInt(REDIS_PORT) : undefined,
-      REDIS_URL,
-    );
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({
+      req,
+    }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
 
-    httpServer.listen(PORT, IP, () => {
-      logger.info(`Server started at ${IP}:${PORT}`);
-    });
-  } catch (err) {
-    logger.error(err);
-    process.exit(0);
-  }
+  app.use('/img', express.static(path.join(__dirname, 'public', 'images')));
+  app.use(graphqlUploadExpress());
+  server.applyMiddleware({ app });
+
+  await db.sequelize.sync({ force: false });
+
+  await new Promise<void>((r) =>
+    httpServer.listen({ port: PORT, host: IP }, r),
+  );
+
+  logger.info(`Server started at ${IP}:${PORT}${server.graphqlPath}`);
 };
