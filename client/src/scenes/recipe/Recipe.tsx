d1352e67787a1d739beea1d4d0dd267642b7f5f3
TyroneWilliams <itenory@gmail.com>
2021-09-29 12:37:54 -0400

    Now displays recipe

--- client/src/scenes/recipe/Recipe.tsx
+++ client/src/scenes/recipe/Recipe.tsx
@@ -1,58 +1,129 @@
 import * as React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+import { useAuthContext } from '../../context/auth';
+import Loading from '../../components/Loading';
 import './styles/recipe.css';
 
-const RecipePage = () => (
-  <section className="recipe">
-    <header className="recipe__header">
-      <h1 className="recipe__title">Recipe title</h1>
-      <div className="recipe__stats">
-        <span className="recipe__">Ratings</span>
-      </div>
-      <div className="recipe__summary">
-        This is a wonderful version of tomato soup.
-      </div>
-      <div className="recipe__author">Author</div>
-    </header>
+const QUERY_RECIPE = gql`
+  query ($recipeId: ID!) {
+    getRecipe(recipeId: $recipeId) {
+      id
+      title
+      summary
+      directions
+      ingredients
+      author
+      authorName
+      mainImage
+      prepTime
+      cookTime
+    }
+  }
+`;
 
-    <div className="recipe__ingredients">
-      <h2 className="recipe__subtitle">Ingredents</h2>
-      <div className="">
-        <div className="recipe__adjustment">
-          <button className="recipe__btn recipe__btn--minus" type="button">
-            -
-          </button>
-          <span className="recipe__servings">12</span>
-          <button className="recipe__btn recipe__btn--plus" type="button">
-            +
-          </button>
-        </div>
-      </div>
-
-      <ul className="recipe__list recipe__list--ingredients">
-        <li className="recipe__item">
-          <span className="recipe__amount">2</span>
-          cups of Flour
-        </li>
-      </ul>
-    </div>
-
-    <div className="recipe__directions">
-      <h3 className="recipe__direction-title">24 Method</h3>
-      <ul className="recipe__list recipe__list--direction">
-        <li className="recipe__item recipe__item--direction">
-          Wash and dry the exterior of each cucumber.
-        </li>
-      </ul>
-    </div>
-
-    <div className="recipe__facts">
-      <h2 className="recipe__subtitle">Nutrition Facts</h2>
-
-      <div className="recipe__nutrition">
-        No facts are availble at this time.
-      </div>
-    </div>
+const RecipeNotFound = () => (
+  <section className="recipe recipe--missing">
+    <div className="recipe__404">This recipe does not exist</div>
+    <Link to="/recipes/popular">Check out more popular recipes here</Link>
   </section>
 );
+
+const RecipePage = () => {
+  const { state } = useAuthContext();
+  const { recipeId } = useParams<{ recipeId: string }>();
+
+  const { loading, error, data } = useQuery(QUERY_RECIPE, {
+    variables: {
+      recipeId,
+    },
+  });
+
+  if (error) return <div>error occurred</div>;
+  if (loading) return <Loading />;
+  if (!data.getRecipe) return <RecipeNotFound />;
+
+  const {
+    id,
+    title,
+    summary,
+    prepTime,
+    cookTime,
+    authorName,
+    author,
+    mainImage,
+  } = data.getRecipe;
+
+  return (
+    <section className="recipe">
+      <div className="recipe__wrapper">
+        <header className="recipe__header">
+          <img
+            className="recipe__img"
+            src={`/img/${mainImage}`}
+            alt="Recipe Example"
+          />
+
+          <h1 className="recipe__title">{title}</h1>
+
+          <div className="recipe__summary">{summary}</div>
+
+          <div className="recipe__stats">
+            <span className="recipe__cook">{cookTime} min</span>
+            <span className="recipe__prep">{prepTime} min</span>
+          </div>
+
+          <div className="recipe__controls">
+            {state.id === author ? (
+              <div className="recipe__options">
+                <Link className="" to={`/recipe/${id}/edit`}>
+                  Edit
+                </Link>
+              </div>
+            ) : null}
+          </div>
+
+          <Link className="recipe__author" to={`/account/profile/${author}`}>
+            <img
+              className="recipe__author-img"
+              src="https://cdn.dribbble.com/users/2527772/screenshots/11920939/media/24291b3fd033123e1507cbff2080e59e.png"
+              alt="User Profile"
+            />
+            <div className="recipe__author-name">{authorName}</div>
+            <div className="recipe__arrow" />
+          </Link>
+        </header>
+
+        <div className="recipe__ingredients">
+          <h2 className="recipe__subtitle">Ingredents</h2>
+
+          <ul className="recipe__list recipe__list--ingredients">
+            <li className="recipe__item">
+              <span className="recipe__amount">2</span>
+              cups of Flour
+            </li>
+          </ul>
+        </div>
+
+        <div className="recipe__directions">
+          <h3 className="recipe__direction-title">24 Method</h3>
+          <ul className="recipe__list recipe__list--direction">
+            <li className="recipe__item recipe__item--direction">
+              Wash and dry the exterior of each cucumber.
+            </li>
+          </ul>
+        </div>
+
+        <div className="recipe__facts">
+          <h2 className="recipe__subtitle">Nutrition Facts</h2>
+
+          <div className="recipe__nutrition">
+            No facts are availble at this time.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default RecipePage;
