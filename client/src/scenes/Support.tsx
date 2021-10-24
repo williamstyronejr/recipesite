e63d8a4cdd7a220a18f815300716ae4c877e6052
williamstyronejr <itenory@gmail.com>
2021-10-24 13:36:23 -0400

    Add links to page

--- client/src/scenes/Support.tsx
+++ client/src/scenes/Support.tsx
@@ -1,21 +1,35 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/support.css';
 
 const SupportPage = () => (
   <section className="support">
     <header className="support__header">
-      <h1 className="support__heading">Support Center</h1>
-
-      <div className="support__container">
-        <input
-          className="support__input"
-          type="text"
-          placeholder="Search for a support problem"
-        />
-      </div>
+      <h1 className="support__heading">
+        Support Center
+        <div className="support__container">
+          <input
+            className="support__input"
+            type="text"
+            placeholder="Search for a support problem"
+          />
+        </div>
+      </h1>
     </header>
 
-    <div className="support__list" />
+    <div className="support__list">
+      <div className="support__item">
+        <Link className="support__link" to="/recovery">
+          Forgot password
+        </Link>
+      </div>
+
+      <div className="support__item">
+        <Link className="support__link" to="/faq">
+          Faq
+        </Link>
+      </div>
+    </div>
   </section>
 );
 
