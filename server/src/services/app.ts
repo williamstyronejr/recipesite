18c676862b13a15b0fa0f9fe7f0818150e999665
williamstyronejr <itenory@gmail.com>
2021-11-12 21:50:46 -0500

    csurf setup

--- server/src/services/app.ts
+++ server/src/services/app.ts
@@ -3,17 +3,18 @@ import cors from 'cors';
 import express from 'express';
 import { graphqlUploadExpress } from 'graphql-upload';
 import cookieParser from 'cookie-parser';
+import csrf from 'csurf';
 const app = express();
 
+const { SESSION_TIMEOUT } = process.env;
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    maxAge: parseInt(SESSION_TIMEOUT as string) || 1800,
+    origin:
+      process.env.NODE_ENV === 'production' ? false : 'http://localhost:3000',
   }),
 );
-
-app.use(graphqlUploadExpress());
-app.use(cookieParser());
 app.use('/img', express.static(path.join(__dirname, '..', 'public', 'images')));
 
 // Static folder
@@ -22,6 +23,24 @@ app.use(
   express.static(
     path.join(__dirname, '..', '..', '..', 'client', 'build', 'static'),
   ),
+);
+
+app.use(graphqlUploadExpress());
+app.use(cookieParser());
+app.use(
+  csrf({
+    cookie: {
+      sameSite: 'lax',
+      secure: true,
+      httpOnly: true,
+    },
+    value: (req) => {
+      const csrfToken = req.headers['csrf-token']
+        ? (req.headers['csrf-token'] as string)
+        : '';
+      return csrfToken;
+    },
+  }),
 );
 
 export default app;
